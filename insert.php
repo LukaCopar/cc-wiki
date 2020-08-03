82bb7d2f277fb1c7ea5c90dcc2dbda5dd5de5729
Luka Copar <luka.copar@gmail.com>
2020-08-03 15:30:20 +0200

    okej to je

--- insert.php
+++ insert.php
@@ -6,15 +6,7 @@
 	$query="SELECT name FROM talents";
 	$resultset = $conn->query($query);
 
-	echo(mysqli_num_rows($resultset));
-
-	while($rows = $resultset->fetch_assoc()){
-
-	echo($rows['name']);
-	foreach ($rows as $row){
-		echo($row);
-	}
-	}
+	//echo(mysqli_num_rows($resultset));
 	?>
 	
 	<html>
@@ -28,14 +20,14 @@
 	<body>
 	    <h1>INSERT HEROES</h1> </br>
 		<form action="/action_page.php">
-  <label for="cars">Choose a car:</label>
+  <label for="cars">Talent:</label>
   <select name="cars" id="cars">
-    <?php
-	$query="SELECT name FROM talents";
-	$result = $conn->query($query);
-	?> <option value="<?php  ?>" >   <?php ?>
 
-
+  <?php 
+	 while($rows = $resultset->fetch_assoc()){
+	foreach ($rows as $row){
+	?>
+	 <option value="<?php echo($row); ?>" > <?php echo($row);} ?></option> <?php } ?>
   </select>
   <br><br>
   <input type="submit" value="Submit">
