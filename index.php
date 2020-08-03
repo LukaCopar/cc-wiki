d55d1f106ea52a2a55dd58d46c0dda272eabce0f
Luka Čopar <luka.copar@gmail.com>
2020-08-03 18:32:27 +0200

    Druga tema

--- index.php
+++ index.php
@@ -5,161 +5,44 @@
 		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
 		<script src="js/jquery.min.js"></script>
 		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/style.css" type="text/css">
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-		</noscript>
+
+		<!-- Compressed CSS -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+
+		<!-- Compressed JavaScript -->
+		<script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
 		<script src="js/index.js"></script>
 		
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+	
 	</head>
 
 	<body>
 	    <div class="wrapper style1">
 			<!-- Header -->
-				<div id="header" class="skel-panels-fixed">
-					<div id="logo">
-						<h1><a href="index.html">Phase Shift</a></h1>
-						<span class="tag">by TEMPLATED</span>
-					</div>
-					<nav id="nav">
-						<ul>
-							<li class="active"><a href="index.html">Homepage</a></li>
-							<li><a href="left-sidebar.html">Left Sidebar</a></li>
-							<li><a href="right-sidebar.html">Right Sidebar</a></li>
-							<li><a href="no-sidebar.html">No Sidebar</a></li>
-						</ul>
-					</nav>
+			<div id="header" class="navbar">
+				<div id="logo">
+					<h1><a href="index.html">Phase Shift</a></h1>
+					<span class="tag">by TEMPLATED</span>
 				</div>
+				<ul id="navbar" class="menu align-center">
+					<li><a class="nav-item active" href="#">Heroes</a></li>
+					<li><a class="nav-item" href="#">Talents</a></li>
+					<li><a class="nav-item" href="#">Pets</a></li>
+					<li><a class="nav-item" href="#">IDK smthn</a></li>
+				</ul>
+			</div>
 			<!-- Extra -->
-			<div id="extra">
+			<div id="content">
 				<div class="container">
 					<div class="row">
-						<div class="hero">
-							<?php 
-								require 'php/heroes.php'
-							?>	
-						</div>					
-						<div class="hero">
-							<p>1Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-							<a href="#" class="button">Read More</a> 
-						</div>
-						<div class="hero">
-							<p>drug Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-							<a href="#" class="button">Read More</a> 
-						</div>
-						<div class="hero">
-							<p>3Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-							<a href="#" class="button">Read More</a> 
-						</div>
-						<div class="hero">
-							<p>4Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-							<a href="#" class="button">Read More</a> 
-						</div>
-						<div class="hero">
-							<p>5onec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum  wisi maecenas ligula.</p>
-							<a href="#" class="button">Read More</a> 
-						</div>	 
-					</div>
-				</div>
-			</div>
-			<!-- Main -->
-			<div id="main">
-				<div class="container">
-					<div class="row"> 
-						
-						<!-- Content -->
-						<div class="6u">
-							<section>
-								<ul class="style">
-									<li class="fa fa-wrench">
-										<h3>Integer ultrices</h3>
-										<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
-									<li class="fa fa-leaf">
-										<h3>Aliquam luctus</h3>
-										<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
-								</ul>
-							</section>
-						</div>
-						<div class="6u">
-							<section>
-								<ul class="style">
-									<li class="fa fa-cogs">
-										<h3>Integer ultrices</h3>
-										<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
-									<li class="fa fa-road">
-										<h3>Aliquam luctus</h3>
-										<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
-								</ul>
-							</section>
-						</div>
+						<?php 
+							require 'php/heroes.php'
+						?>					
 					</div>
 				</div>
 			</div>
 
-	</div>
-		<!-- Footer -->
-		<div id="footer" class="wrapper style2">
-			<div class="container">
-				<section>
-					<header class="major">
-						<h2>Mauris vulputate dolor</h2>
-						<span class="byline">Integer sit amet pede vel arcu aliquet pretium</span>
-					</header>
-					<form method="post" action="#">
-						<div class="row half">
-							<div class="12u">
-								<input class="text" type="text" name="name" id="name" placeholder="Name" />
-							</div>
-						</div>
-						<div class="row half">
-							<div class="12u">
-								<input class="text" type="text" name="email" id="email" placeholder="Email" />
-							</div>
-						</div>
-						<div class="row half">
-							<div class="12u">
-								<textarea name="message" id="message" placeholder="Message"></textarea>
-							</div>
-						</div>
-						<div class="row half">
-							<div class="12u">
-								<ul class="actions">
-									<li>
-										<input type="submit" value="Send Message" class="button alt" />
-									</li>
-								</ul>
-							</div>
-						</div>
-					</form>
-				</section>
-			</div>
 		</div>
-
-		<!-- Copyright -->
-		<div id="copyright">
-			<div class="container">
-				<div class="copyright">
-					<p>Design: <a href="http://templated.co">TEMPLATED</a> Images: <a href="http://unsplash.com">Unsplash</a> (<a href="http://unsplash.com/cc0">CC0</a>)</p>
-					<ul class="icons">
-						<li><a href="#" class="fa fa-facebook"><span>Facebook</span></a></li>
-						<li><a href="#" class="fa fa-twitter"><span>Twitter</span></a></li>
-						<li><a href="#" class="fa fa-google-plus"><span>Google+</span></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-	    <h1>ayy</h1>
-	    <button id="move">Za gor avkm</button>
-	    <div>
-	    	<div class="nav-item" id="heroes"><a href="#">Heroes</a></div>
-	    	<div class="nav-item" id="talents"><a href="#">Talents</a></div>
-	    	<div class="nav-item" id="pets"><a href="#">Pets</a></div>
-	    </div>
-		<a href="./insert.php">INSERT</a>
 	</body>
 </html>
