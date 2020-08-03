d1ee544c8943eb97c91c60e50b36dc2819a3f51a
Luka Copar <luka.copar@gmail.com>
2020-08-03 18:31:12 +0200

    header

---
+++ header.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	    <title>Castle Clash Wiki</title>
+		
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="./css/style.css" type="text/css">
+		<script src="js/index.js"></script>
+		
+	</head>
+
+	<body>
+	  <div></div>
+	</body>
+</html>
