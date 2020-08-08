d17a4a32dec34ec487573fa3d2b953d8217fc798
Luka Čopar <luka.copar@gmail.com>
2020-08-08 18:25:49 +0200

    mal vec local storaga

--- js/index.js
+++ js/index.js
@@ -5,12 +5,23 @@ $(document).ready(() => {
 	$(function() {
 		FastClick.attach(document.body);
 	});
-
-	$.ajax({
-		url: "/php/heroes.php"
-	}).done(function(c) {
-		heroes = JSON.parse(c);
-	});
+	if(window.localStorage.getItem("heroes") == null) {
+		$.ajax({
+			url: "/php/heroes.php"
+		}).done(function(c) {
+			heroes = JSON.parse(c);
+			var ins = [];
+			heroes.forEach(function(val) {
+				if(window.localStorage.getItem(val.name) == null) {
+					ins.push(val);
+				}
+			});
+			window.localStorage.setItem("heroes", JSON.stringify(ins));
+		});
+	}
+	else {
+		heroes = JSON.parse(window.localStorage.getItem("heroes"));
+	}
 
 	$.ajax({
 		url: "/php/talents.php"
@@ -171,7 +182,28 @@ $(document).ready(() => {
 			parent.append(form);
 		});
 	});
-
+	$(window).on('resize', function(el) {
+		if(!$("#navbar").hasClass("animate") && $(e.target.parentNode).hasClass("active")) {
+			$("#navbar").animate({
+				height: window.innerHeight * 0.09
+			}, 200, function () {
+				$("#meni").css("bottom", "0");
+				$("#navbar").css("overflow", "hidden");
+			});
+		}
+		else {
+			$("#navbar").animate({
+				height: window.innerHeight
+			}, 200, function () {
+				$("#meni").css("position", "fixed");
+				$("#meni").css("top", "0");
+				$("#meni").css("width", "100%");
+				$("#meni").css("z-index", "100");
+				$("#meni").css("height", "9vh");
+				$("#navbar").css("overflow", "auto");
+			});
+		}
+	});
 	$(".nav-item").click(function (e) {
 		$("#navbar").scrollTop(0);
 		if ($("#navbar").hasClass("animate") && $(e.target.parentNode).hasClass("active")) {
