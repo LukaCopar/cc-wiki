c9ca09c402257fba231a704d19017e0a8e497208
Luka Čopar <luka.copar@gmail.com>
2020-08-19 14:10:53 +0200

    lmao2

--- js/heroes.js
+++ js/heroes.js
@@ -18,23 +18,23 @@ class Hero {
 	this.skill = 0;
 
 	constructor(id, img_url, bio, name, skill_img, img_url_evo, warden, mov_spd, atk_spd, atk, hp, acc, ddg, crt, crr, crd) {
-		this.id = (typeofid === 'undefined') ? 0 : id;
-		this.name = (typeofname === 'undefined') ? '' : name;
-		this.biography = (typeofbio === 'undefined') ? '' : bio;
-		this.img_url = (typeofimg_url === 'undefined') ? '' : img_url;
-		this.img_url_evo = (typeofimg_url_evo === 'undefined') ? '' : img_url;
-		this.skill_img = (typeofskill_img === 'undefined') ? '' : skill_img;
-		this.warden = (typeofwarden === 'undefined') ? null : warden;
-		this.mov_spd = (typeofmov_spd === 'undefined') ? 0 : mov_spd;
-		this.atk_spd = (typeofatk_spd === 'undefined') ? 0 : atk_spd;
-		this.atk = (typeofatk === 'undefined') ? 0 : atk;
-		this.hp = (typeofhp === 'undefined') ? 0 : hp;
-		this.acc = (typeofacc === 'undefined') ? 0 : acc;
-		this.ddg = (typeofddg === 'undefined') ? 0 : ddg;
-		this.crt = (typeofcrt === 'undefined') ? 0 : crt;
-		this.crr = (typeofcrr === 'undefined') ? 0 : crr;
-		this.crd = (typeofcrd === 'undefined') ? 0 : crd;
-		this.skill = (typeofskill === 'undefined') ? null : skill;
+		this.id = (typeof id === 'undefined') ? 0 : id;
+		this.name = (typeof name === 'undefined') ? '' : name;
+		this.biography = (typeof bio === 'undefined') ? '' : bio;
+		this.img_url = (typeof img_url === 'undefined') ? '' : img_url;
+		this.img_url_evo = (typeof img_url_evo === 'undefined') ? '' : img_url;
+		this.skill_img = (typeof skill_img === 'undefined') ? '' : skill_img;
+		this.warden = (typeof warden === 'undefined') ? null : warden;
+		this.mov_spd = (typeof mov_spd === 'undefined') ? 0 : mov_spd;
+		this.atk_spd = (typeof atk_spd === 'undefined') ? 0 : atk_spd;
+		this.atk = (typeof atk === 'undefined') ? 0 : atk;
+		this.hp = (typeof hp === 'undefined') ? 0 : hp;
+		this.acc = (typeof acc === 'undefined') ? 0 : acc;
+		this.ddg = (typeof ddg === 'undefined') ? 0 : ddg;
+		this.crt = (typeof crt === 'undefined') ? 0 : crt;
+		this.crr = (typeof crr === 'undefined') ? 0 : crr;
+		this.crd = (typeof crd === 'undefined') ? 0 : crd;
+		this.skill = (typeof skill === 'undefined') ? null : skill;
 
 	}
 	output() {
